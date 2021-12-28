0de4a583cc3a149ee9aec6380895070eb804e3ac
Zeehan0133 <zyne0133@gmail.com>
2021-12-28 14:02:15 +0530

    [Zeehan] Added (error handling stored procedure mysql file)

--- Error_handling_sp.sql
+++ Error_handling_sp.sql
@@ -24,24 +24,22 @@ DROP PROCEDURE IF EXISTS spGetExp;
 
 delimiter //
 create procedure spGetExp(
-    IN inid int)
+    IN inid int, IN f1 int, IN f2 int)
      BEGIN
       DECLARE EXIT HANDLER FOR 1062
     BEGIN
- 	SELECT CONCAT('Duplicate key (',inid,') occurred') AS message;
+ 	SELECT CONCAT('Duplicate key (',inid,',',f1,',',f2,') occurred') AS message;
     END;
-     insert into user_roles(user_id)
-     values(inid);
-     SELECT SUM(user_id)
-     FROM user_roles
-     WHERE user_id=inid;
+     insert into mentor_techstack(id,mentor_id,tech_stack_id)
+     values(inid,f1,f2);
+     SELECT COUNT(*)
+     FROM mentor_techstack
+     WHERE id=inid;
+           
      
      END //
      delimiter ;
-
-
-
-
+     
 -- handler precedence
 DROP PROCEDURE IF EXISTS spGetExp; 
 
@@ -53,15 +51,14 @@ create procedure spGetExp(
     DECLARE EXIT HANDLER FOR SQLEXCEPTION SELECT 'SQLException encountered' Message; 
     DECLARE EXIT HANDLER FOR SQLSTATE '23000' SELECT 'SQLSTATE 23000' ErrorCode;
      
-     insert into user_roles(user_id)
+     insert into mentor(id)
      values(inid);
-     SELECT SUM(user_id)
-     FROM user_roles
-     WHERE user_id=inid;
+     SELECT SUM(id)
+     FROM mentor
+     WHERE id=inid;
      
      END //
      delimiter ;
-
 
 -- declare condition 
 
